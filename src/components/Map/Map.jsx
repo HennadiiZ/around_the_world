c13b031a101c0576a7b7a7b2f6c59bc54b3a519c
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-10-04 18:08:16 -0600

    clean up comments

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -9,35 +9,16 @@ export default function Map() {
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
   const navigate = useNavigate();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  // const position = [51.505, -0.09];
   const { cities, isLoading } = useContext(CitiesContext);
 
   return (
-    // <div className={styles.mapContainer} onClick={() => navigate('form')}>
-    //   Map{lat}
-    //   {lng}
-    //   <button
-    //     onClick={() => {
-    //       setSearchParams({ lat: 23, lng: 50 });
-    //     }}
-    //   >
-    //     Change pos
-    //   </button>
-    // </div>
-
     <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
         center={mapPosition}
         zoom={13}
         scrollWheelZoom={true}
-        // >
-        //   <TileLayer
-        //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        //     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        //   />
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
